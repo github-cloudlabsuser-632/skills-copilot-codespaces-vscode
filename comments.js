a399bc36a779e013dda9001dcebc4feb31bf968e
github-cloudlabsuser-632 <167329616+github-cloudlabsuser-632@users.noreply.github.com>
2024-07-06 12:59:12 +0000

    modified:   comments.js

--- comments.js
+++ comments.js
@@ -1,65 +1,22 @@
 //create web server
+//create a new instance of http server
+//require http module
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
-
-//create server
-http.createServer(function (request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            fs.readdir('./data', function (error, filelist) {
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = '<ul>';
-                var i = 0;
-                while (i < filelist.length) {
-                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                    i = i + 1;
-                }
-                list = list + '</ul>';
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        } else {
-            fs.readdir('./data', function (error, filelist) {
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                    var title = queryData.id;
-                    var list = '<ul>';
-                    var i = 0;
-                    while (i < filelist.length) {
-                        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                        i = i + 1;
-                    }
-                    list = list + '</ul>';
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</
+var fs = require('fs');
+var path = require('path');
+//create a new instance of http server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+//
