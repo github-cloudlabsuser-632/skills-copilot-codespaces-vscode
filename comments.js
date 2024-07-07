fc76f0283d31033f03ae46661d5e0d36a5a23834
github-cloudlabsuser-632 <167329616+github-cloudlabsuser-632@users.noreply.github.com>
2024-07-07 19:24:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,11 @@
-//create web server
-//create a new instance of http server
-//require http module
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
-//create a new instance of http server
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);  
-//
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
